45a001780661237c1f972dd6551f9d02f90c045c
Daniel Nachun <daniel.nachun@gmail.com>
2025-07-04 11:22:26 -0700

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 # Create package archive and install globally
 npm pack --ignore-scripts
 npm install -ddd \
+    --no-bin-links \
     --global \
     --build-from-source \
     ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}.tgz
@@ -18,6 +19,13 @@ jq 'del(.devDependencies)' package.json.bak > package.json
 pnpm install
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
+mkdir -p ${PREFIX}/bin
+tee ${PREFIX}/bin/shopify << EOF
+#!/bin/sh
+exec \${CONDA_PREFIX}/lib/node_modules/@shopify/cli/bin/run.js "\$@"
+EOF
+chmod +x ${PREFIX}/bin/shopify
+
 tee ${PREFIX}/bin/shopify.cmd << EOF
-call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\bin\shopify %*
+call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\lib\node_modules\@shopify\cli\bin\run.js %*
 EOF
